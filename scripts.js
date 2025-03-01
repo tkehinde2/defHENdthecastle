724fe64d7a9618a53249c774119accd4752debcd
romir <romir@wifi-roaming-128-4-180-115.wifi.udel.edu>
2025-03-01 16:14:24 -0500

    UI changes

--- scripts.js
+++ scripts.js
@@ -1,25 +1,26 @@
-var castleName;
-
 const intro = document.getElementById("intro");
-const castle = document.getElementById("castle")
+const castle = document.getElementById("castle");
 const grass = document.getElementById("grass");
 const checklist = document.getElementById("checklist");
 const achievements = document.getElementById("achievements");
 
 window.onload = function() {
-    intro.classList.add('introDown'); // Add the animation class when the page loads
+  intro.classList.add('introDown');
 }
 
+function start() {
+  const castleName = document.getElementById("castleName").value;
+  if (!castleName) {
+    alert("Please enter a castle name!");
+    return;
+  }
 
-function start(){
-    castleName = document.getElementById("castleName").value;   
+  intro.classList.remove("introDown");
+  intro.classList.add("introUp");
 
-    intro.classList.remove("introDown");
-    intro.classList.add("introUp");
+  grass.classList.add("grassDown");
+  castle.classList.add("landCastle");
 
-    grass.classList.add("grassDown");
-    castle.classList.add("landCastle")
-
-    checklist.classList.add("showMenus");
-    achievements.classList.add("showMenus");
+  checklist.classList.add("showMenus");
+  achievements.classList.add("showMenus");
 }
